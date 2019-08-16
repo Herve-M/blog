2a814753f59f8172022b7d65267bb57d50baaa4f
MATYSIAK Hervé <fossdev@hervematysiak.eu>
2019-08-16 13:26:45 +0700

    Add `html-minifier` to build .travis, updated to latest linux env.

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,4 +3,5 @@ then
   hugo -b https://hervematysiak.eu/ --buildExpired --cleanDestinationDir
 else
   hugo -b https://beta.hervematysiak.eu/ --buildDrafts --buildExpired --buildFuture --cleanDestinationDir
+  html-minifier -c html-minifier.conf --input-dir public/ --output-dir public/ --file-ext html
 fi
