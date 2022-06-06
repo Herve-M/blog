d3d2bdb48dae8fbd2eebaf1699f767905a8a8412
MATYSIAK Hervé <fossdev@hervematysiak.eu>
2022-06-06 21:07:09 +0700

    feat: remove Travis.org related files

--- .travis/build.sh
+++
@@ -1,7 +0,0 @@
-if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]
-then
-  hugo -b https://hervematysiak.eu/ --buildExpired --cleanDestinationDir
-else
-  hugo -b https://beta.hervematysiak.eu/ --buildDrafts --buildExpired --buildFuture --cleanDestinationDir
-  html-minifier -c html-minifier.conf --input-dir public/ --output-dir public/ --file-ext html
-fi
